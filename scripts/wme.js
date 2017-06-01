81d2b71d4785aeacff117c474827b769330d6728
Scottt <scott.geyer@eagles.usm.edu>
2017-06-01 00:46:46 -0500

    Update wme.js

--- scripts/wme.js
+++ scripts/wme.js
@@ -15,14 +15,29 @@ var candidates = [ {
   "party": "Libertarian",
   "stance1" : 2
   }
- ]
+ ];
 
 Questions = [ {
   "Question" : "Should goods and services be provided by force?",
-  "Response1" : ["Absolutely",-2],
-  "Response2" : ["Yes", -1],
-  "Response3" : ["Maybe", 0],
-  "Response4" : ["No", 1],
-  "Response5" : ["Never", 2]
+  "Responses" : [
+    ["Absolutely",-2],
+    ["Yes", -1],
+    ["Maybe", 0],
+    ["No", 1],
+    ["Never", 2]]
 }
-]
+];
+
+function loadQuestions() {
+  var chunk = '';
+  for (i = 0; i < Questions.length; i++) {
+    chunk += '<p class="question">' + Questions[i].Question + '</p>';
+    chunk += '<p class="answers">";
+    for (j = 0; j < Questions[i].Responses.length; j++) {
+     var qr = Questions[i].Responses[j];
+     chunk += '<button onclick="tally(' + i + ',' + qr[1] + ')">' + qr[0] + '</button>';
+    }
+    document.getElementById("questionArea") = chunk;
+    
+  }
+}
