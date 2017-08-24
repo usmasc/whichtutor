02befb5cb3963d46547aae0150263f1f10628555
Scottt <scott.geyer@eagles.usm.edu>
2017-08-24 18:39:25 -0500

    Update tutors.js

--- scripts/tutors.js
+++ scripts/tutors.js
@@ -1,5 +1,5 @@
-var prefPoints = 5; // points for preferred subject
-var subPoints = 10; // points for subject
+var prefPoints = 3; // points for preferred subject
+var subPoints = 5; // points for subject
 var OQpoints = 1; // other question points
 
 var tutors = [
@@ -12,10 +12,11 @@ var tutors = [
     morning: [],
     turtle: [],
     artist: [],
-    pony: ['Apple Jack'],
+    pony: ["Apple Jack"],
     titan: [],
     score: 0,
-    bio: "Adam is a Geography major. He tutors Geography, speaking, and writing. He also likes to code. He's a Veteran. He's involved with GTU and Southern Connection. Adam prefers that you hit reply and not reply all when you reply to emails."  
+    bio:
+      "Adam is a Geography major. He tutors Geography, speaking, and writing. He also likes to code. He's a Veteran. He's involved with GTU and Southern Connection. Adam prefers that you hit reply and not reply all when you reply to emails."
   },
   {
     name: "Aleysh",
@@ -29,7 +30,8 @@ var tutors = [
     pony: [],
     titan: [],
     score: 0,
-    bio: "Aleysh is a Chemistry tutor. She is also involved in Sigma Mu: Science and Math Club"
+    bio:
+      "Aleysh is a Chemistry tutor. She is also involved in Sigma Mu: Science and Math Club"
   },
   {
     name: "Casey",
@@ -38,9 +40,9 @@ var tutors = [
     prefsub: [],
     cite: [],
     morning: ["morning run"],
-    turtle: ['Michelangelo'],
+    turtle: ["Michelangelo"],
     artist: [],
-    pony: ['Pinkie Pie'],
+    pony: ["Pinkie Pie"],
     titan: [],
     score: 0,
     bio: "Casey is Casey. Once you get to know him, you'll understand."
@@ -48,7 +50,14 @@ var tutors = [
   {
     name: "Dane",
     main: ["math", "writing"],
-    subjects: ["math", "Calculus","Algebra", "Intermediate Algebra", "College Algebra", "Business Calculus"],
+    subjects: [
+      "math",
+      "Calculus",
+      "Algebra",
+      "Intermediate Algebra",
+      "College Algebra",
+      "Business Calculus"
+    ],
     prefsub: [],
     cite: [],
     morning: [],
@@ -57,12 +66,19 @@ var tutors = [
     pony: [],
     titan: [],
     score: 0,
-    bio: "Dane is a Chemical Engineer who is going back to school to learn Computer Science. He wants to learn all the things!"
+    bio:
+      "Dane is a Chemical Engineer who is going back to school to learn Computer Science. He wants to learn all the things!"
   },
   {
     name: "Elizabeth",
     main: ["math"],
-    subjects: ["math", "Algebra", "Intermediate Algebra", "College Algebra", "Business Calculus"],
+    subjects: [
+      "math",
+      "Algebra",
+      "Intermediate Algebra",
+      "College Algebra",
+      "Business Calculus"
+    ],
     prefsub: ["Intermediate Algebra", "College Algebra", "Business Calculus"],
     cite: [],
     morning: ["hot tea"],
@@ -71,7 +87,8 @@ var tutors = [
     pony: ["Fluttershy"],
     titan: ["Starfire"],
     score: 0,
-    bio: "Elizabeth is a marketing major who enjoys tutoring Intermediate Algebra, College Algebra, and Brief Applied Calculus (Business Calculus)."
+    bio:
+      "Elizabeth is a marketing major who enjoys tutoring Intermediate Algebra, College Algebra, and Brief Applied Calculus (Business Calculus)."
   },
   {
     name: "Grace",
@@ -93,10 +110,10 @@ var tutors = [
     subjects: ["Bilogy"],
     prefsub: ["Bilogy"],
     cite: [],
-    morning: ['coffee'],
-    turtle: ['Michelangelo'],
-    artist: ['Leonardo'],
-    pony: ['Twilight Sparkle'],
+    morning: ["coffee"],
+    turtle: ["Michelangelo"],
+    artist: ["Leonardo"],
+    pony: ["Twilight Sparkle"],
     titan: ["Robin"],
     score: 0,
     bio: "Heather can't wait to help you learn."
@@ -132,7 +149,7 @@ var tutors = [
   {
     name: "Samantha",
     main: ["Chemistry"],
-    subjects: ["Chemistry", "CHEM 100", "CHEM 104","General Chemistry"],
+    subjects: ["Chemistry", "CHEM 100", "CHEM 104", "General Chemistry"],
     prefsub: ["Chemistry"],
     cite: [],
     morning: ["coffee"],
@@ -165,7 +182,8 @@ var tutors = [
       "stats",
       "Calculus",
       "Algebra",
-      "Intermediate Algebra", "College Algebra",
+      "Intermediate Algebra",
+      "College Algebra",
       "Business Calculus",
       "business stats"
     ],
@@ -177,7 +195,8 @@ var tutors = [
     pony: ["Rainbow Dash"],
     titan: ["Raven"],
     score: 0,
-    bio: "Scott is an Air Force Veteran. He wants to teach math and a little bit of programming too. He made this quiz."
+    bio:
+      "Scott is an Air Force Veteran. He wants to teach math and a little bit of programming too. He made this quiz."
   },
   {
     name: "Teneala",
@@ -191,7 +210,8 @@ var tutors = [
     pony: [],
     titan: [],
     score: 0,
-    bio: "Teneala is majoring in Computer Science with a double minor in Spanish and Math as part of her plot for world domination. We're not kidding."
+    bio:
+      "Teneala is majoring in Computer Science with a double minor in Spanish and Math as part of her plot for world domination. We're not kidding."
   },
   {
     name: "Veronica",
@@ -205,21 +225,23 @@ var tutors = [
     pony: [],
     titan: [],
     score: 0,
-    bio: "Veronica is a Veteran. She is now majoring in Geography. She was an accounting major, but Geography is awesome. So there!"
+    bio:
+      "Veronica is a Veteran. She is now majoring in Geography. She was an accounting major, but Geography is awesome. So there!"
   },
   {
     name: "Zachary Ransom",
     main: ["speaking", "writing", "Psychology"],
-    subjects: ["speaking", "writing", "Psychology","Academic Research"],
+    subjects: ["speaking", "writing", "Psychology", "Academic Research"],
     prefsub: ["Psychology"],
     cite: ["APA"],
-    morning: ['coffee','bacon'],
-    turtle: ['Raphael'],
-    artist: ['Michelangelo'],
-    pony: ['Twilight Sparkle'],
-    titan: ['Cyborg'],
+    morning: ["coffee", "bacon"],
+    turtle: ["Raphael"],
+    artist: ["Michelangelo"],
+    pony: ["Twilight Sparkle"],
+    titan: ["Cyborg"],
     score: 0,
-    bio: "<p>Zachary Ransom is currently a Senior majoring in Psychology.  His focus within the field of psychology is currently focused on Applied Behavioral Analysis with the intent of pursuing a Ph.D. in School Psychology.  In accordance with these goals, he is currently a clinical intern and conducting the research for his senior thesis at USM's Autism Project.</p><p>A desire to serve his community led him to become a paramedic and now serves as his inspiration to push forward with his current academic pursuits.   This also serves as his inspiration to be a tutor.  He takes pride in being able to offer my assistance in helping his fellow students fulfill their academic potential and achieve success.  His strategy for tutoring centers around this concept.  His main goal is to help those that he tutors realize what strategies work well for them and to then help them hone those talents.</p>"
+    bio:
+      "<p>Zachary Ransom is currently a Senior majoring in Psychology.  His focus within the field of psychology is currently focused on Applied Behavioral Analysis with the intent of pursuing a Ph.D. in School Psychology.  In accordance with these goals, he is currently a clinical intern and conducting the research for his senior thesis at USM's Autism Project.</p><p>A desire to serve his community led him to become a paramedic and now serves as his inspiration to push forward with his current academic pursuits.   This also serves as his inspiration to be a tutor.  He takes pride in being able to offer my assistance in helping his fellow students fulfill their academic potential and achieve success.  His strategy for tutoring centers around this concept.  His main goal is to help those that he tutors realize what strategies work well for them and to then help them hone those talents.</p>"
   },
   {
     name: "Zachary Stewart",
@@ -245,7 +267,7 @@ var Questions = [
   {
     id: "morning",
     question: "Your favorite part of your morning routine",
-    responses: ["coffee", "morning run", "bacon","hot tea"]
+    responses: ["coffee", "morning run", "bacon", "hot tea"]
   },
   {
     id: "turtle",
@@ -281,14 +303,14 @@ var resultsArea = document.getElementById("results");
 
 // object to store responses
 var responses = {
-  'main': "",
-  'subject': "",
-  'cite': "",
-  'morning': "",
-  'turtle': "",
-  'artist': "",
-  'pony': "",
-  'titan': ""
+  main: "",
+  subject: "",
+  cite: "",
+  morning: "",
+  turtle: "",
+  artist: "",
+  pony: "",
+  titan: ""
 };
 
 // first question to determine main domain
@@ -360,7 +382,8 @@ var mathQuestions = [
       "Trigonometry",
       "Business Calculus",
       "business stats",
-      "Intermediate Algebra", "College Algebra",
+      "Intermediate Algebra",
+      "College Algebra",
       "other"
     ]
   }
@@ -416,8 +439,10 @@ function loadQuestions(chunck) {
     } // close i loop
     chunck += "</p>";
   } // close q loop
-  chunck += '<p><button onclick="scoreIt()" class="scorebtn">Score It!</button> ';
-  chunck += '<button onclick="loadFirstQuestion()" class="scorebtn">Reset</button></p>';
+  chunck +=
+    '<p><button onclick="scoreIt()" class="scorebtn">Score It!</button> ';
+  chunck +=
+    '<button onclick="loadFirstQuestion()" class="scorebtn">Reset</button></p>';
   questionArea.innerHTML = chunck;
 } // close question function
 
@@ -447,7 +472,7 @@ function loadSecondQuestions(set, setStr) {
 function setQ(set, q, i) {
   var thingie = set[q].id;
   responses[set[q].id] = set[q].responses[i];
-    /* test code
+  /* test code
     var testThing = 'main: ' + responses.main + ', ';
     testThing += 'subject: ' + responses.subject + ', ';
     testThing += 'citation: ' + responses.cite + ', ';
@@ -500,7 +525,10 @@ function scoreIt() {
     }
   }
   // document.getElementById("test").innerHTML =   tutors[tutorIndex].name + " " + highScore;
-  chunck = "<h1>You should make an appointment with " + tutors[tutorIndex].name + "</h1>";
+  chunck =
+    "<h1>You should make an appointment with " +
+    tutors[tutorIndex].name +
+    "</h1>";
   chunck += "<p>" + tutors[tutorIndex].bio + "</p>";
   resultsArea.innerHTML = chunck;
 } // close score it function
